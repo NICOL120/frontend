295ff77d63e4435ee0479cd7e1574893f6a28398
NICOL1201 <117902091+NICOL120@users.noreply.github.com>
2023-01-30 22:47:26 +0800

    Update schema_all.sh

--- schema_all.sh
+++ schema_all.sh
@@ -1,37 +1,14 @@
-npx json2ts -i ../contracts/contracts/spectrum_gov/schema/ -o ./src/app/services/api/gov
-npx json2ts -i ../contracts/contracts/farms/spectrum_mirror_farm/schema/ -o ./src/app/services/api/mirror_farm
-npx json2ts -i ../contracts/contracts/farms/spectrum_spec_farm/schema/ -o ./src/app/services/api/spec_farm
-npx json2ts -i ../contracts/contracts/spectrum_staker/schema/ -o ./src/app/services/api/staker
-npx json2ts -i ../contracts/contracts/spectrum_token/schema/ -o ./src/app/services/api/token
-npx json2ts -i ../contracts/contracts/spectrum_platform/schema/ -o ./src/app/services/api/platform
-npx json2ts -i ../contracts/contracts/spectrum_wallet/schema/ -o ./src/app/services/api/wallet
-npx json2ts -i ../contracts/contracts/farms/spectrum_anchor_farm/schema/ -o ./src/app/services/api/anchor_farm
-npx json2ts -i ../contracts/contracts/farms/spectrum_pylon_farm/schema/ -o ./src/app/services/api/pylon_farm
-npx json2ts -i ../contracts/contracts/farms/spectrum_terraworld_farm/schema/ -o ./src/app/services/api/terraworld_farm
-npx json2ts -i ../contracts/contracts/farms/spectrum_valkyrie_farm/schema/ -o ./src/app/services/api/valkyrie_farm
-npx json2ts -i ../contracts/contracts/farms/spectrum_orion_farm/schema/ -o ./src/app/services/api/orion_farm
-npx json2ts -i ../contracts/contracts/farms/spectrum_nexus_farm/schema/ -o ./src/app/services/api/nexus_farm
-npx json2ts -i ../contracts/contracts/farms/spectrum_nexus_nasset_psi_farm/schema/ -o ./src/app/services/api/nexus_nassets_psi_farm
-npx json2ts -i ../contracts/contracts/farms/spectrum_pylon_liquid_farm/schema/ -o ./src/app/services/api/pylon_liquid_farm
-npx json2ts -i ../contracts/contracts/farms/spectrum_terra_name_service_farm/schema/ -o ./src/app/services/api/terra_name_service_farm
-npx json2ts -i ../contracts/contracts/farms/spectrum_glow_farm/schema/ -o ./src/app/services/api/glow_farm
-npx json2ts -i ../contracts/contracts/farms/spectrum_loterra_farm/schema/ -o ./src/app/services/api/loterra_farm
-npx json2ts -i ../contracts/contracts/farms/spectrum_nasset_farm/schema/ -o ./src/app/services/api/nasset_farm
-
-npx json2ts -i ../contracts/contracts/astro_farms/spectrum_astroport_farm/schema/ -o ./src/app/services/api/astroport_farm
-npx json2ts -i ../contracts/contracts/astro_farms/spectrum_astroport_luna_ust_farm/schema/ -o ./src/app/services/api/astroport_luna_ust_farm
-npx json2ts -i ../contracts/contracts/astro_farms/spectrum_astroport_token_luna_farm/schema/ -o ./src/app/services/api/astroport_token_luna_farm
-npx json2ts -i ../contracts/contracts/astro_farms/spectrum_astroport_token_ust_farm/schema/ -o ./src/app/services/api/astroport_token_ust_farm
-npx json2ts -i ../contracts/contracts/astro_farms/spectrum_astroport_token_token_farm/schema/ -o ./src/app/services/api/astroport_token_token_farm
-
-npx json2ts -i ../contracts/contracts/astro_farms/spectrum_astroport_stluna_luna_farm/schema/ -o ./src/app/services/api/astroport_stluna_luna_farm
-npx json2ts -i ../contracts/contracts/astro_farms/spectrum_astroport_sttoken_ust_farm/schema/ -o ./src/app/services/api/astroport_sttoken_ust_farm
-npx json2ts -i ../contracts/contracts/astro_farms/spectrum_astroport_weldo_stluna_farm/schema/ -o ./src/app/services/api/astroport_weldo_stluna_farm
-
-
-npx json2ts -i ../astroport/astroport/contracts/factory/schema/ -o ./src/app/services/api/astroport_factory
-npx json2ts -i ../astroport/astroport/contracts/pair/schema/ -o ./src/app/services/api/astroport_pair
-npx json2ts -i ../astroport/astroport/contracts/pair_stable/schema/ -o ./src/app/services/api/astroport_pair_stable
-npx json2ts -i ../astroport/astroport/contracts/router/schema/ -o ./src/app/services/api/astroport_router
-npx json2ts -i ../astroport/astroport/contracts/token/schema/ -o ./src/app/services/api/astroport_token
+npx json2ts -i ../astroport/astroport-core/contracts/factory/schema/ -o ./src/app/services/api/astroport_factory
+npx json2ts -i ../astroport/astroport-core/contracts/pair/schema/ -o ./src/app/services/api/astroport_pair
+npx json2ts -i ../astroport/astroport-core/contracts/pair_stable/schema/ -o ./src/app/services/api/astroport_pair_stable
+npx json2ts -i ../astroport/astroport-core/contracts/router/schema/ -o ./src/app/services/api/astroport_router
+npx json2ts -i ../astroport/astroport-core/contracts/token/schema/ -o ./src/app/services/api/token
 npx json2ts -i ../astroport/astroport-core/contracts/tokenomics/generator/schema/ -o ./src/app/services/api/astroport_generator
+
+npx json2ts -i ../spectrum-core/contracts/astroport_farm/schema -o ./src/app/services/api/spectrum_astroport_farm
+npx json2ts -i ../spectrum-core/contracts/compound_proxy/schema -o ./src/app/services/api/spectrum_compound_proxy
+npx json2ts -i ../contracts-private/contracts/spectrum_farmv2/schema -o ./src/app/services/api/spectrum_spec_farm
+
+
+
+npx json2ts -i ../spectrum-core/contracts/fees_collector/schema -o ./common/services/api/spectrum_fees_collector
